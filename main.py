519816955f44c5925a34cb74bf199e7fc414d6c1
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-20 09:52:40 +0100

    Added answers

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+def gambler( s, n, p ) :
+    while( s>0 and s<n ) :
+       if np.random.uniform(0,1)<p : s = s + 1
+       else : s = s - 1
+
+    if s==n : return 0 
+    elif s==0 : return 1 
+    else : assert(False)
